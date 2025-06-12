38bebe4121d790175388d2bd1966d9f63113f452
Tanushree Dwibedi <130269480+tanushree-dwibedi@users.noreply.github.com>
2025-06-12 23:50:23 +0530

    Add files via upload

---
+++ NodeAssignment/Assignment1/r.mjs
@@ -0,0 +1,6 @@
+import * as mf from './oa.mjs';
+console.log(mf.hello());
+console.log(`${mf.next()} ${mf.squared()}`);
+
+console.log(`${mf.default()} ${mf.squared()}`);
+console.log(`${mf.squared()}+3`);
