f62705a91f4d5cdc84344069ac83486ec315f3c7
Tanushree Dwibedi <130269480+tanushree-dwibedi@users.noreply.github.com>
2025-06-20 01:06:03 +0530

    Add files via upload

---
+++ NodeAssignment/Assignment2/cube.mjs
@@ -0,0 +1,24 @@
+// cubeVolume.mjs
+
+// Function to calculate volume of a cube
+export function volumeOfCube(side) {
+    return Math.pow(side, 3); // side³
+  }
+  
+
+  /*
+  function volumeOfCube(side) {
+  return Math.pow(side, 3);
+}
+
+function surfaceAreaOfCube(side) {
+  return 6 * side * side;
+}
+
+export { volumeOfCube, surfaceAreaOfCube };
+*/
+
+/*
+const volumeOfCube = (side) => side ** 3;
+export default volumeOfCube;
+*/
